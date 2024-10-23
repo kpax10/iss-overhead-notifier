551d0040c09df5e2550599607aafc08194fbe294
Kyle <notxap55@yahoo.com>
2024-10-23 10:43:13 -0700

    refactored

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ MY_EMAIL = "timmyt345345@gmail.com"
 MY_PASSWORD = "qbtnsxioriyqxijz"
 
 
-#Your position is within +5 or -5 degrees of the ISS position.
 def is_within_range():
     print('checking...')
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -22,6 +21,16 @@ def is_within_range():
 
 
 def is_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
     time_now = datetime.now(timezone.utc)
     return sunset < time_now.hour < sunrise
 
@@ -37,30 +46,8 @@ def send_email():
         )
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-# print('time now:', time_now, 'sunrise:', sunrise, 'sunset:', sunset)
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
-
 while True:
-
     if is_within_range() and is_dark():
         send_email()
         time.sleep(21600)
-
     time.sleep(60)
-
